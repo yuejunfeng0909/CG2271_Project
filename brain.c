13aab9c8ca24dd4acce80c1f8e26f120f400669e
Zhang Haoyu <haoyusimon927@gmail.com>
2022-03-20 23:35:12 +0800

    update brain.c

--- brain.c
+++ brain.c
@@ -1,8 +1,25 @@
 #include "brain.h"
 
+#include "MKL25Z4.h"                    // Device header
+#include "UART_control.h"
+#include "led_control.h"
+
+#define MOTOR_COMMAND 0b1u
+#define SPEED_COMMAND 0b10u
+#define LED_COMMAND 0b11u
+#define BUZZER_COMMAND 0b100u
+#define SELF_DRIVING_COMMAND 0b101u
+
 osSemaphoreId_t brain_sem;
 
-void initBrain(void) {
+extern osSemaphoreId_t LED_sem;
+extern osSemaphoreId_t motor_sem;
+extern osSemaphoreId_t speed_sem;
+extern osSemaphoreId_t audio_sem;
+extern osSemaphoreId_t sd_sem;
+
+
+void Init_brain(void) {
 	brain_sem = osSemaphoreNew(1, 0, NULL);
 }
 
@@ -14,6 +31,19 @@ void brain_thread(void *arguments) {
 			case LED_COMMAND:
 				osSemaphoreRelease(LED_sem);
 				break;
+			case MOTOR_COMMAND:
+				osSemaphoreRelease(motor_sem);
+				break;
+			case SPEED_COMMAND:
+				osSemaphoreRelease(speed_sem);
+				break;
+			case BUZZER_COMMAND:
+				osSemaphoreRelease(audio_sem);
+				break;
+			case SELF_DRIVING_COMMAND:
+				osSemaphoreRelease(sd_sem);
+				break;
 		}
 	}
 }
+
