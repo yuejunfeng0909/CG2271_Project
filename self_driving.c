9f968a8d7faa48dbaa82c2bf9236cde1e8340f37
Yurui <69446663+ShaoYurui@users.noreply.github.com>
2022-04-07 15:43:18 +0800

    Update self_driving.c

--- self_driving.c
+++ self_driving.c
@@ -11,51 +11,50 @@
 osSemaphoreId_t selfDriving;
 
 volatile int US_STOP_DISNTANCE = 20;
-volatile int US_DIS = 20;
+volatile int US_DIS = 99;
 int isAfterSelfDriving = 0;
 
 void approach(void)
 {
-	
-		setMotion_and_Speed(FORWARD,50);
-	
 		US_DIS = measure_distance_cm();
 		
-		while((US_DIS> (3 * US_STOP_DISNTANCE)) || (US_DIS == -1))
+		setMotion_and_Speed(FORWARD,50);
+	
+
+		while((US_DIS> (3 * US_STOP_DISNTANCE)) || (US_DIS < 0 ))
 		{
 			US_DIS = measure_distance_cm();
 		}
 				
 		setMotion_and_Speed(FORWARD,20);
-		while((US_DIS> (2 * US_STOP_DISNTANCE)) || (US_DIS == -1)) {US_DIS = measure_distance_cm();}
+		while((US_DIS> (2 * US_STOP_DISNTANCE)) || (US_DIS < 0 )) {US_DIS = measure_distance_cm();}
 				
 		setMotion_and_Speed(FORWARD,10);
-		while((US_DIS> (1 * US_STOP_DISNTANCE)) || (US_DIS == -1)) {US_DIS = measure_distance_cm();}
+		while((US_DIS> (1 * US_STOP_DISNTANCE)) || (US_DIS < 0 )) {US_DIS = measure_distance_cm();}
 				
 		setMotion(STOP);
-		osDelay(500);
+		osDelay(100);
 }
 
 void run_around_obstacle(void)
 {
 	turn_degree(SPIN_RIGHT,90);
-	forward_distance(40);
+	forward_distance(25);
 	turn_degree(SPIN_LEFT,90);
-	forward_distance(80);
+	forward_distance(50);
 	turn_degree(SPIN_LEFT,90);
-	forward_distance(80);
+	forward_distance(50);
 	turn_degree(SPIN_LEFT,90);
-//	forward_distance(80);
-//	turn_degree(SPIN_LEFT,90);
-//	forward_distance(40);
-//	turn_degree(SPIN_RIGHT,90);
+
 }
 
 void self_driving(void *argument)
 {
 	// add semaphore
+	US_DIS = 99;
+	isAfterSelfDriving = 0;
 	approach();
 	run_around_obstacle();
-	forward_distance(400);
+	forward_distance(300);
 	isAfterSelfDriving = 1;
 }
